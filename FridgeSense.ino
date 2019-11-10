8e58a969b78067b35477bfeb23d5ea8d76171306
Jordan Smith <jdudeisme3000@gmail.com>
2019-11-10 15:43:38 -0500

    Update FridgeSense.ino
    changed digital read to touch read

--- FridgeSense.ino
+++ FridgeSense.ino
@@ -11,10 +11,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  valFridge = digitalRead(Fridge);
+  valFridge = touchRead(Fridge);
 
   Serial.println(valFridge);
 
   delay(1000);
 
 }
+
+//When the sensor is touched, the values go down. Would need a threshold to determine when the sensor is touched
