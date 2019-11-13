5faad5b4fce5fdbf9385ac26f2c09fc0f42d8c04
TanTan Wang <tantan.wang@yale.edu>
2019-11-12 22:48:25 -0500

    multiple sensors accounted for - revamped code

--- ultraSense/ultraSense.ino
+++ ultraSense/ultraSense.ino
@@ -1,24 +1,40 @@
-const int trigPin = 4;
-const int echoPin = 16;
+const int trigPin1 = 4;
+const int echoPin1 = 16;
 
-float duration, distance;
+const int trigPin2 = 25;
+const int echoPin2 = 26;
 
-void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
+long duration, distance, BackSensor,FrontSensor;
+
+void setup()
+{
+Serial.begin (9600);
+pinMode(trigPin1, OUTPUT);
+pinMode(echoPin1, INPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin2, INPUT);
+
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+SonarSensor(trigPin1, echoPin1);
+FrontSensor = distance;
+SonarSensor(trigPin2, echoPin2);
+BackSensor = distance;
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(100);
+Serial.print(FrontSensor);
+Serial.print(" - ");
+Serial.println(BackSensor);
+}
+
+void SonarSensor(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+
 }
